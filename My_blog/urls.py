02da99efd4a2ada9094244fbdd354ecc27e76b63
Valentin Albornoz <valengo2017@gmail.com>
2022-12-14 16:20:25 -0300

    entrega

---
+++ My_blog/urls.py
@@ -0,0 +1,32 @@
+"""proyectoFinal URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from .views import mostrar_curso_html, mostrar_index, mostrar_referencias, mostrat_repaso, crear_curso, crear_profesor, buscar_comision, buscar_profesor, crear_estudiante, buscar_estudiante
+
+
+urlpatterns = [
+    path('mostrar_curso_html/', mostrar_curso_html, name='Mostrar'),
+    path('', mostrar_index),
+    path('mostrar_referencias/', mostrar_referencias, name='Referencias'),
+    path('mostrar_repaso/', mostrat_repaso, name='Repaso'),
+    path('crear_curso/', crear_curso, name='Crear Curso'),
+    path('crear_profesor/', crear_profesor, name='Crear Profesor'),
+    path('buscar_comision/', buscar_comision, name='Buscar Comision'),
+    path('buscar_profesor/', buscar_profesor, name='Buscar Profesor'),
+    path('crear_estudiante/', crear_estudiante, name='Crear Estudiante'),
+    path('buscar_estudiante/', buscar_estudiante, name='Buscar Estudiante'),
+
+]
---
+++ README.md
@@ -0,0 +1,4 @@
+Este es un proyecto que se realizó para el curso de Python en CoderHouse, el mismo consiste en un blog para crear profesores, alumnos y cursos
+
+Cada funcion tiene un boton, para crear cursos, alumnos y profesor
+y un boton para buscar los mismos, el curso se busca por comision, tanto alumnos como profesores se buscan por email.
